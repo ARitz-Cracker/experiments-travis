799e363f13bcd5b5a565c7d3e7a2d168e0babbee
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2019-02-20 11:47:10 -0500

    Testing xenial, network isolation

--- start.js
+++ start.js
@@ -1,33 +1,24 @@
 const {spawn} = require("child_process");
-const geth = spawn("geth", [
-	"--rpc",
-	"--rpcaddr",
-	"127.65.82.67",
-	"--rpcapi",
-	"eth,net,web3",
-	"--ws",
-	"--wsaddr",
-	"127.65.82.67",
-	"--wsapi",
-	"eth,net,web3",
-	"--rpccorsdomain=\"*\"",
-	"--datadir",
-	"chain1010",
-	"--networkid",
-	"1010",
-	"--mine",
-	"--minerthreads=1",
-	"--gasprice",
-	"0",
-	"--etherbase=0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf",
-	"--ethash.dagdir",
-	"mine1010"
+const geth = spawn("sg", [
+	"no-internet",
+	"-c",
+	"geth --rpc --rpcaddr 127.65.82.67 --rpcapi eth,net,web3 --ws --wsaddr 127.65.82.67 --wsapi eth,net,web3 --rpccorsdomain=\"*\" --datadir chain1010 --networkid 1010 --mine --minerthreads=1 --gasprice 0 --etherbase=0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf --ethash.dagdir mine1010"
 ],{stdio:"inherit"});
+const {Web3Connection} = require("arc-web3");
+const web3 = new Web3Connection("http://127.65.82.67:8545");
 const f = async function(){
 	try{
-		await new Promise((resolve, reject) => {
-			setTimeout(resolve, 10 * 60 * 1000)
-		});
+		let endTime = Date.now() + (20 * 60 * 1000);
+		while (Date.now() < endTime){
+			try{
+				console.log("ARCWeb3: "+(await web3.blockNumber()));
+			}catch(ex){
+				console.log("ARCWeb3: "+ex.name+": "+ex.message);
+			}
+			await new Promise((resolve, reject) => {
+				setTimeout(resolve, 4000);
+			});
+		}
 		geth.kill();
 	}catch(ex){
 		console.error(ex.stack);

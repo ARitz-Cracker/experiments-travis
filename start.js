0f1fc815afb8c3d2877aadd0f32126851f81e504
Aritz Beobide-Cardinal <aritz@aritzcracker.ca>
2019-02-20 12:18:00 -0500

    Why don't we try disabling geth networking from geth itself??

--- start.js
+++ start.js
@@ -1,9 +1,7 @@
 const {spawn} = require("child_process");
-const geth = spawn("sudo", [
-	"-g",
-	"no-internet",
-	//"geth --rpc --rpcaddr 127.65.82.67 --rpcapi eth,net,web3 --ws --wsaddr 127.65.82.67 --wsapi eth,net,web3 --rpccorsdomain=\"*\" --datadir chain1010 --networkid 1010 --mine --minerthreads=1 --gasprice 0 --etherbase=0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf --ethash.dagdir mine1010"
-	"geth",
+const geth = spawn("geth", [
+	"--maxpeers",
+	"0",
 	"--rpc",
 	"--rpcaddr",
 	"127.65.82.67",
